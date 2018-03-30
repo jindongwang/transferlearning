3cb1b81fa1eda66ebebb4f57f48663c2fb78a0ff
jindongwang <jindongwang@outlook.com>
2018-03-30 14:19:26 +0800

    mod: DaNN

--- code/deep/DaNN/data_loader.py
+++ code/deep/DaNN/data_loader.py
@@ -14,6 +14,22 @@ def load_data(root_dir,domain,batch_size):
             root=root_dir + domain,
             transform=transform
         )
-    data_loader = torch.utils.data.DataLoader(dataset=image_folder,batch_size=batch_size,shuffle=True,num_workers=2
+    data_loader = torch.utils.data.DataLoader(dataset=image_folder,batch_size=batch_size,shuffle=True,num_workers=2,drop_last=True
     )
+    return data_loader
+
+def load_test(root_dir,domain,batch_size):
+    transform = transforms.Compose([
+        transforms.Grayscale(),
+        transforms.Resize([28, 28]),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0, 0, 0), std=(1, 1, 1)),
+    ]
+    )
+    image_folder = datasets.ImageFolder(
+        root=root_dir + domain,
+        transform=transform
+    )
+    data_loader = torch.utils.data.DataLoader(dataset=image_folder, batch_size=batch_size, shuffle=False, num_workers=2
+                                              )
     return data_loader
