66f67e387cb33812cc34e0e494cc0905d6360b32
jindongwang <jindongwang@outlook.com>
2018-11-14 17:04:28 +0800

    upd: BDA python code

--- code/traditional/BDA/BDA.py
+++ code/traditional/BDA/BDA.py
@@ -1,4 +1,4 @@
-#encoding=utf-8
+# encoding=utf-8
 """
     Created on 9:52 2018/11/14 
     @author: Jindong Wang
@@ -15,6 +15,8 @@ import scipy.io
 import scipy.linalg
 import sklearn.neighbors
 import sklearn.metrics
+from sklearn import svm
+from sklearn import metrics
 
 
 def kernel(ker, X, X2, gamma):
@@ -43,44 +45,43 @@ def kernel(ker, X, X2, gamma):
         K = np.exp(-gamma * np.arccos(D) ** 2)
     return K
 
-from sklearn import svm
 
-def proxy_a_distance(source_X, target_X, verbose=False):
+def proxy_a_distance(source_X, target_X):
     """
     Compute the Proxy-A-Distance of a source/target representation
     """
     nb_source = np.shape(source_X)[0]
     nb_target = np.shape(target_X)[0]
 
-    if verbose:
-        print('PAD on', (nb_source, nb_target), 'examples')
+    train_X = np.vstack((source_X, target_X))
+    train_Y = np.hstack((np.zeros(nb_source, dtype=int), np.ones(nb_target, dtype=int)))
 
-    C_list = np.logspace(-5, 4, 10)
+    clf = svm.LinearSVC()
+    clf.fit(train_X, train_Y)
+    y_pred = clf.predict(train_X)
+    error = metrics.mean_absolute_error(train_Y, y_pred)
+    dist = 2 * (1 - 2 * error)
+    return dist
 
-    half_source, half_target = int(nb_source/2), int(nb_target/2)
-    train_X = np.vstack((source_X[0:half_source, :], target_X[0:half_target, :]))
-    train_Y = np.hstack((np.zeros(half_source, dtype=int), np.ones(half_target, dtype=int)))
 
-    test_X = np.vstack((source_X[half_source:, :], target_X[half_target:, :]))
-    test_Y = np.hstack((np.zeros(nb_source - half_source, dtype=int), np.ones(nb_target - half_target, dtype=int)))
-
-    best_risk = 1.0
-    for C in C_list:
-        clf = svm.SVC(C=C, kernel='linear', verbose=False)
-        clf.fit(train_X, train_Y)
-
-        train_risk = np.mean(clf.predict(train_X) != train_Y)
-        test_risk = np.mean(clf.predict(test_X) != test_Y)
-
-        if verbose:
-            print('[ PAD C = %f ] train risk: %f  test risk: %f' % (C, train_risk, test_risk))
-
-        if test_risk > .5:
-            test_risk = 1. - test_risk
-
-        best_risk = min(best_risk, test_risk)
-
-    return 2 * (1. - 2 * best_risk)
+def estimate_mu(_X1, _Y1, _X2, _Y2):
+    adist_m = proxy_a_distance(_X1, _X2)
+    C = len(np.unique(_Y1))
+    epsilon = 1e-3
+    list_adist_c = []
+    for i in range(1, C + 1):
+        ind_i, ind_j = np.where(_Y1 == i), np.where(_Y2 == i)
+        Xsi = _X1[ind_i[0], :]
+        Xtj = _X2[ind_j[0], :]
+        adist_i = proxy_a_distance(Xsi, Xtj)
+        list_adist_c.append(adist_i)
+    adist_c = sum(list_adist_c) / C
+    mu = adist_c / (adist_c + adist_m)
+    if mu > 1:
+        mu = 1
+    if mu < epsilon:
+        mu = 0
+    return mu
 
 
 class BDA:
@@ -148,7 +149,7 @@ class BDA:
                     N = N + np.dot(e, e.T)
             if self.mu == -1.0:
                 if Xs_new is not None:
-                    mu = proxy_a_distance(Xs_new, Xt_new)
+                    mu = estimate_mu(Xs_new, self.Ys, Xt_new, Y_tar_pseudo)
                 else:
                     mu = 0
             M = (1 - mu) * M + mu * N
