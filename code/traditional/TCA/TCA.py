cbaf3f761666b74239d3cf3978dd12e398dc9e1f
Orvin Demsy <orvindemsy@gmail.com>
2021-04-01 09:08:47 +0900

    incrmntal target data

--- code/traditional/TCA/TCA.py
+++ code/traditional/TCA/TCA.py
@@ -8,6 +8,7 @@ import scipy.io
 import scipy.linalg
 import sklearn.metrics
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
 
 def kernel(ker, X1, X2, gamma):
@@ -40,18 +41,21 @@ class TCA:
         self.dim = dim
         self.lamb = lamb
         self.gamma = gamma
+        self.W_proj = None
+        self.X_new_src  = None
+        self.clf = None
 
-    def fit(self, Xs, Xt):
+    def fit(self, Xs, Xt1):
         '''
-        Transform Xs and Xt
+        Transform Xs and Xt1
         :param Xs: ns * n_feature, source feature
-        :param Xt: nt * n_feature, target feature
+        :param Xt1: nt * n_feature, target feature
         :return: Xs_new and Xt_new after TCA
         '''
-        X = np.hstack((Xs.T, Xt.T))
+        X = np.hstack((Xs.T, Xt1.T))
         X /= np.linalg.norm(X, axis=0)
         m, n = X.shape
-        ns, nt = len(Xs), len(Xt)
+        ns, nt = len(Xs), len(Xt1)
         e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
         M = e * e.T
         M = M / np.linalg.norm(M, 'fro')
@@ -61,13 +65,20 @@ class TCA:
         a, b = np.linalg.multi_dot([K, M, K.T]) + self.lamb * np.eye(n_eye), np.linalg.multi_dot([K, H, K.T])
         w, V = scipy.linalg.eig(a, b)
         ind = np.argsort(w)
+        
+        # Projecting to latent space
         A = V[:, ind[:self.dim]]
         Z = np.dot(A.T, K)
         Z /= np.linalg.norm(Z, axis=0)
-        Xs_new, Xt_new = Z[:, :ns].T, Z[:, ns:].T
-        return Xs_new, Xt_new
+        
+        Xs_new, Xt1_new = Z[:, :ns].T, Z[:, ns:].T
+        
+        self.W_proj = A
+        self.X_new_src = X
+        
+        return Xs_new, Xt1_new
 
-    def fit_predict(self, Xs, Ys, Xt, Yt):
+    def fit_predict(self, Xs, Ys, Xt1, Yt1):
         '''
         Transform Xs and Xt, then make predictions on target using 1NN
         :param Xs: ns * n_feature, source feature
@@ -76,23 +87,74 @@ class TCA:
         :param Yt: nt * 1, target label
         :return: Accuracy and predicted_labels on the target domain
         '''
-        Xs_new, Xt_new = self.fit(Xs, Xt)
-        clf = KNeighborsClassifier(n_neighbors=1)
+        Xs_new, Xt1_new = self.fit(Xs, Xt1)
+        clf = KNeighborsClassifier(n_neighbors=3)
+        
         clf.fit(Xs_new, Ys.ravel())
-        y_pred = clf.predict(Xt_new)
-        acc = sklearn.metrics.accuracy_score(Yt, y_pred)
+        y_pred = clf.predict(Xt1_new)
+        acc = sklearn.metrics.accuracy_score(Yt1, y_pred)
+        
+        self.clf = clf
         return acc, y_pred
 
+    def fit_target(self, Xt2):
+        '''
+        Map new Xt to the latent space create from self.fit using existing projection matrix
+        This Xt2 is different from Xt1 in the .fit
+        :param Xt : n_s, n_feature, new target feature
+        '''
+        if not np.all(self.W_proj):
+            raise AssertionError('No projection matrix found, use .fit to find new feature of source and target')
+        
+        # Reshape to make it consistent with other methods
+        Xt2 = Xt2.T
+        
+        # Compute kernel with respect to self.X_new_src
+        K = kernel(self.kernel_type, X1 = Xt2, X2 = self.X_new_src, gamma=self.gamma)
+        
+        # New target features
+        Xt2_new = K @ self.W_proj
+        
+        return Xt2_new
+    
+    def fit_predict_target(self, Xt2, Yt2):
+        '''
+        Predict new target from self.fit_target using existing classifier built in .fit_predict
+        Transform Xt2, then make predictions on target using 1NN
+        :param Xt2: nt * n_feature, target feature
+        :param Yt2: nt * 1, target label
+        :return: Accuracy and predicted_labels on the target domain
+        '''
+        if not np.all(self.W_proj) or not np.all(self.clf) :
+            raise AssertionError('No classifier found, trained classifier first using .fit_predict')
+        
+        Xt2_new = self.fit_target(Xt2)
+        
+        y_pred = self.clf.predict(Xt2_new)
+        acc = sklearn.metrics.accuracy_score(Yt2, y_pred)
+        
+        return acc, y_pred
+    
+    
 
 if __name__ == '__main__':
     domains = ['caltech.mat', 'amazon.mat', 'webcam.mat', 'dslr.mat']
-    for i in [2]:
-        for j in [3]:
+    for i in [1]:
+        for j in [2]:
             if i != j:
                 src, tar = 'data/' + domains[i], 'data/' + domains[j]
                 src_domain, tar_domain = scipy.io.loadmat(src), scipy.io.loadmat(tar)
-                Xs, Ys, Xt, Yt = src_domain['feas'], src_domain['label'], tar_domain['feas'], tar_domain['label']
+                Xs, Ys, Xt, Yt = src_domain['feas'], src_domain['labels'], tar_domain['feas'], tar_domain['labels']
+                
+                # Split target data
+                Xt1, Xt2, Yt1, Yt2  = train_test_split(Xt, Yt, train_size=50, stratify=Yt, random_state=42)
+                
+                # Create latent space and evaluate using Xs and Xt1
                 tca = TCA(kernel_type='linear', dim=30, lamb=1, gamma=1)
-                acc, ypre = tca.fit_predict(Xs, Ys, Xt, Yt)
-                print(acc)
-                # It should print 0.910828025477707
+                acc1, ypre1 = tca.fit_predict(Xs, Ys, Xt1, Yt1)
+                
+                # Project and evaluate Xt2 existing projection matrix and classifier
+                acc2, ypre2 = tca.fit_predict_target(Xt2, Yt2)
+                
+    print(f'Accuracy of mapped source and target1 data : {acc1:.3f}') #0.800
+    print(f'Accuracy of mapped target2 data            : {acc2:.3f}') #0.714
